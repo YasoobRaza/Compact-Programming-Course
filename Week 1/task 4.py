bda626307b70d7a43a72e2c530f21181392c0155
Yasoob Raza <yasoob.raza7@gmail.com>
2026-09-22 20:12:36 +0500

    updated week1 task 4

--- Week 1/task 4.py
+++ Week 1/task 4.py
@@ -1,12 +1,9 @@
-import math
-
 if __name__ == '__main__':
-    try:
-        num = int(input("Enter a non-negative integer to calculate its factorial: "))
-        if num < 0:
-            print("Factorial is not defined for negative numbers.")
-        else:
-            fact = math.factorial(num)
-            print(f"The factorial of {num} is {fact}")
-    except ValueError:
-        print("Please enter a valid integer.")
+    num = int(input("Enter a non-negative integer to calculate its factorial: "))
+    if num < 0:
+        print("Factorial is not defined for negative numbers.")
+    else:
+        fact = 1
+        for i in range(1, num + 1):
+            fact *= i
+        print(f"The factorial of {num} is {fact}")
